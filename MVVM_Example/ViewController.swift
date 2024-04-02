75719b5146f17bd381bf133c9b1fd4c334bf2114
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2024-04-02 08:58:10 +0300

    Initial Commit

---
+++ MVVM_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Example
+//
+//  Created by Fazli Altun on 2.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
